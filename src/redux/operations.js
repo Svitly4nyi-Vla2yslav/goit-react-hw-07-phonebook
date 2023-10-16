ca407400c5a22272dfc58d087c1521d3e3926374
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-10-16 20:58:03 +0200

    selector

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -19,3 +19,16 @@ export const fetchTasks = createAsyncThunk(
     }
   }
 );
+
+
+export const deleteTask = createAsyncThunk(
+    "tasks/deleteTask",
+    async (taskId, thunkAPI) => {
+      try {
+        const response = await axios.delete(`/tasks/${taskId}`);
+        return response.data;
+      } catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
+      }
+    }
+  );
