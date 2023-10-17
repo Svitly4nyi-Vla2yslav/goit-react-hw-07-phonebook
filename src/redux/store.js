91290e4c32a566f56c841a41be31a42cb30d6c14
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-10-17 23:31:44 +0200

    store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,23 @@ import { configureStore} from '@reduxjs/toolkit';
 import persistStore from 'redux-persist/es/persistStore';
 import { persistReducerContacts } from './reducer';
 
+// const customMiddleware = (state) => {
+//   return (next) => {
+//     return (action) => {
+//       return next(action)
+//     }
+//   }
+// }
+
 export const store = configureStore({
   reducer: {
     contacts: persistReducerContacts,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false, // вимкнення перевірки на серіалізованість
-    }),
+  // middleware: [customMiddleware],
 });
 export const persistor = persistStore(store);
+
+// getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: false, // вимкнення перевірки на серіалізованість
+//     })
